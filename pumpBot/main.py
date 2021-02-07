720a9c14d54d05aac1ecb37c0c80226b8028074e
paulo <pauloxti@gmail.com>
2021-02-07 21:48:49 +0100

    cancel order

--- pumpBot/main.py
+++ pumpBot/main.py
@@ -135,6 +135,39 @@ def floatPrecision(f, n):
     return str(int(f)) if int(n) == 0 else f
 
 
+# `pattern` is a regex, see https://docs.python.org/3/library/re.html
+# Use https://regexone.com/ if you want a more interactive way of learning.
+#
+# "(?i)" makes it case-insensitive, and | separates "options".
+@client.on(events.NewMessage())#pattern=r'(?i).*\b(hello|hi)\b'))
+async def handler(event):
+    sender = await event.get_sender()
+    #client.get_input_entity(PeerChannel(fwd.from_id))
+    #channel = await event.get_channel()
+
+    #group = event.group()
+    #group = event.get_group()
+    #print(group)
+    #print(utils.get_peer_id(sender))
+    #print(utils.get_input_location(sender))
+    #print(utils.get_input_dialog(sender))
+    #print(utils.get_inner_text(sender))
+
+    #print(utils.get_extension(sender))
+    #print(utils.get_attributes(sender))
+    #print(utils.get_input_user(sender))
+
+    name = utils.get_display_name(sender)
+    print('said', event.text)
+    coin = get_coin_name(event.txt)
+
+    if len(coin) > 0:
+        print("buy")
+
+
+    #print(utils.get_input_entity(PeerChannel(sender)))
+    #print(utils.get_input_channel(get_input_peer(channel)))
+
 apikey = 'z8oJ86HRXRKHppUeLZMOY8564f3gnNueSrmOL1455SXtkTmyHwusLc1XCjjGBKZt'
 secret = 'UZggnxZ7moBpHw74iGK9SkXHlnci6RAsajO7x1wptsGvgr2qs5lRNu6y5WvJZvDJ'
 
@@ -262,7 +295,11 @@ if __name__ == "__main__":
             except:
                 print("Not found!")
                 
-            
+        try:
+            print('(Press Ctrl+C to stop this)')
+            client.run_until_disconnected()
+        finally:
+            client.disconnect()    
 
         try:
             o = client.get_order(symbol=selectedSymbol,orderId=order["orderId"])

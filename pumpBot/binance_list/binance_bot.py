701dd4c714e013477c6a50e64415fdf8e4f71915
Paulo <pgsoares@telecomboutique.com>
2021-03-19 18:36:26 +0100

    bot listin ok

--- pumpBot/binance_list/binance_bot.py
+++ pumpBot/binance_list/binance_bot.py
@@ -10,7 +10,7 @@ from binance.client import Client
 import math
 import webbrowser
 from price_parser import Price
-from decimal import *
+import decimal
 
 
 def check_coin():
@@ -37,13 +37,13 @@ def print_current_status(price, last_price, high_price, stop_loss_price, initial
     else:
         current_price_color = "dark_orange_3a"
 
-    print(stylize("Current Price: " + str(price), colored.fg(current_price_color)))
-    print(stylize("High Price: " + str(high_price), colored.fg("green")))
-    print(stylize("Stop Loss Price: " + str(stop_loss_price), colored.fg("red")))
+    print(stylize("Current Price: " + str(price_from_string(price)), colored.fg(current_price_color)))
+    print(stylize("High Price: " + str(price_from_string(high_price)), colored.fg("green")))
+    print(stylize("Stop Loss Price: " + str(price_from_string(stop_loss_price)), colored.fg("red")))
     print(stylize("Selected Symbol Initial Buy Price: " +
-                  str(initial_buy_price), colored.fg("blue")))
+                  str(price_from_string(initial_buy_price)), colored.fg("blue")))
     print(stylize("selectedSymbolSellPrice: " +
-                  str(take_profit_price), colored.fg("green")))
+                  str(price_from_string(take_profit_price)), colored.fg("green")))
     print("")
 
 
@@ -54,16 +54,16 @@ def floatPrecision(f, n):
     return str(int(f)) if int(n) == 0 else f
 
 
-def cancel_order(order):
+def cancel_order(orderId):
     try:
-        if (order != None):
+        if orderId is not None:
             order_info = client.get_order(
-                symbol=selected_symbol, orderId=order["orderId"])
+                symbol=symbol, orderId=orderId)
             print("order_info")
             print(order_info)
 
             cancel_order_result = client.cancel_order(
-                symbol=selected_symbol, orderId=order_info["orderId"])
+                symbol=symbol, orderId=order_info["orderId"])
             print("cancel_order_result")
             print(cancel_order_result)
             return True
@@ -73,8 +73,15 @@ def cancel_order(order):
     return False
 
 
+def price_from_string(price):
+    p = decimal.Decimal(price)
+    decimal.getcontext().prec = 8
+    decimal.getcontext().create_decimal(p)
+    return p
+
+
 # VARIABLES
-stop_loss_percent = 0.1
+stop_loss_percent = 0.01
 take_profit_percent = 2.0
 initial_price = 0.0
 stop_loss_price = 0.0
@@ -89,8 +96,6 @@ buy_quantity = 0.0
 buy_quantity_with_fee = 0.0
 symbol_info = {}
 # selectedSymbolName = sys.argv[1]
-selected_symbol_name = "eth"
-selected_symbol = selected_symbol_name.upper() + "BTC"
 apikey = 'z8oJ86HRXRKHppUeLZMOY8564f3gnNueSrmOL1455SXtkTmyHwusLc1XCjjGBKZt'
 secret = 'UZggnxZ7moBpHw74iGK9SkXHlnci6RAsajO7x1wptsGvgr2qs5lRNu6y5WvJZvDJ'
 client = Client(apikey, secret)
@@ -98,10 +103,11 @@ buy = False
 symbol_name = "eth"
 symbol = symbol_name.upper() + "BTC"
 max_profit_percent = 100
-order = None
+
 pres = 0
 if __name__ == "__main__":
     filename = "asset.txt"
+    coin = None
     while True:
         coin = check_coin()
         if coin is not None:
@@ -110,7 +116,8 @@ if __name__ == "__main__":
             webbrowser.open("https://www.binance.com/es/trade/" + coin.upper() + "_BTC?layout=pro", new=2)
             break
 
-    btc_balance = float(client.get_asset_balance(asset="BTC")['free'])
+    btc_balance = price_from_string(client.get_asset_balance(asset="BTC")['free'])
+    selected_symbol_balance = price_from_string(client.get_asset_balance(asset=coin.upper())['free'])
 
     print("btcBalance")
     print(btc_balance)
@@ -153,11 +160,17 @@ if __name__ == "__main__":
     print(stop_loss_price)
     print("")
 
-    initial_buy_price = 1.02
+    initial_buy_price = price_from_string("0.00002144")
+
+    print(initial_buy_price)
     high_price = initial_buy_price
     print("sell_quantity")
     print(sell_quantity)
     print("")
+
+    order = None
+    orderId = None
+
     while True:
         # GET PRICE
         symbol_info = client.get_symbol_info(symbol)
@@ -174,30 +187,39 @@ if __name__ == "__main__":
         if current_price > initial_buy_price and current_price > high_price:
             high_price = current_price
             print(stylize("New price target: " + str(current_price), colored.fg("yellow")))
-            target_sell_price = float(round(float(current_price) - (float(current_price) * 0.2), 8))
-            target_sell_stop_price = float(round(float(current_price) - (float(current_price) * 0.3), 8))
+            target_sell_price = float(round(float(current_price) - (float(current_price) * 0.01), 8))
+            target_sell_stop_price = float(round(float(current_price) - (float(current_price) * 0.02), 8))
             print("price " + str(target_sell_price))
 
-            cancel_order(order)
+            print("order[orderId]")
+
+            if order is not None:
+                print("order[orderId]")
+                print(order["orderId"])
+
+            cancel_order(orderId)
             order = client.create_order(symbol=symbol,
                                         side="SELL",
                                         type="STOP_LOSS_LIMIT",
-                                        quantity=Decimal(sell_quantity),
-                                        price=str(target_sell_stop_price),
-                                        stopPrice=str(target_sell_price),
+                                        quantity=decimal.Decimal(sell_quantity),
+                                        price=floatPrecision(target_sell_stop_price, tick_size),
+                                        stopPrice=floatPrecision(target_sell_price, tick_size),
                                         timeInForce="GTC")
 
+            orderId = order["orderId"]
+            print("order[orderId]")
+            print(order["orderId"])
         # STOP LOSS 5%
         if current_price <= stop_loss_price:
-            cancel_order(order)
-            order = client.order_market_sell(symbol=symbol, quantity=Decimal(sell_quantity))
+            cancel_order(orderId)
+            order = client.order_market_sell(symbol=symbol, quantity=decimal.Decimal(sell_quantity))
             print("STOP LOSS")
             exit(0)
 
         # TAKE MAX PROFIT
         if current_price > initial_buy_price + ((initial_buy_price * max_profit_percent) / 100):
-            cancel_order(order)
-            order = client.order_market_sell(symbol=symbol, quantity=Decimal(sell_quantity))
+            cancel_order(orderId)
+            order = client.order_market_sell(symbol=symbol, quantity=decimal.Decimal(sell_quantity))
             print("TAKE MAX PROFIT")
             exit(0)
 
